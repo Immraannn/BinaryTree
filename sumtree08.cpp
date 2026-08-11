d6b9bcaf8d3b10127344de60cb812d03f1e84b1f
Md Imran Shakeel <imshkl2005@gmail.com>
2026-08-11 22:17:52 +0530

    Update sumtree08.cpp

--- sumtree08.cpp
+++ sumtree08.cpp
@@ -1,49 +1,102 @@
-pair<bool, int> isSumTreeFast(Node* root)
+#include <iostream>
+using namespace std;
+
+class node
 {
-    // Base case: Empty tree
+public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int value)
+    {
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+// Function to check whether the tree is a Sum Tree
+bool isSumTree(node* root)
+{
+    // Empty tree is a Sum Tree
     if (root == NULL)
-    {
-        pair<bool, int> p = make_pair(true, 0);
-        return p;
-    }
+        return true;
 
-    // Base case: Leaf node
+
+    // Leaf node is automatically a Sum Tree
     if (root->left == NULL && root->right == NULL)
-    {
-        pair<bool, int> p = make_pair(true, root->data);
-        return p;
-    }
+        return true;
 
-    // Get result from left subtree
-    pair<bool, int> leftAns = isSumTreeFast(root->left);
 
-    // Get result from right subtree
-    pair<bool, int> rightAns = isSumTreeFast(root->right);
+    // Find sum of left subtree
+    int leftSum = sum(root->left);
 
-    bool left = leftAns.first;
-    bool right = rightAns.first;
 
-    // Check Sum Tree property at current node
-    bool condn = (root->data == leftAns.second + rightAns.second);
+    // Find sum of right subtree
+    int rightSum = sum(root->right);
 
-    pair<bool, int> ans;
 
-    if (left && right && condn)
-    {
-        ans.first = true;
+    // Current node must equal
+    // sum of left and right subtrees
+    if (root->data != leftSum + rightSum)
+        return false;
 
-        // Sum of current subtree
-        ans.second = 2 * root->data;
-    }
-    else
-    {
-        ans.first = false;
-    }
 
-    return ans;
+    // Check left subtree
+    bool left = isSumTree(root->left);
+
+
+    // Check right subtree
+    bool right = isSumTree(root->right);
+
+
+    // Both subtrees must also be Sum Trees
+    return left && right;
 }
 
-bool isSumTree(Node* root)
+
+// Function to calculate sum of all nodes
+int sum(node* root)
 {
-    return isSumTreeFast(root).first;
+    // Empty tree has sum 0
+    if (root == NULL)
+        return 0;
+
+    return root->data
+         + sum(root->left)
+         + sum(root->right);
+}
+
+
+int main()
+{
+    /*
+              16
+             /  \
+           10    6
+          / \   / \
+         4   6 3   3
+    */
+
+    node* root = new node(16);
+
+    root->left = new node(10);
+    root->right = new node(6);
+
+    root->left->left = new node(4);
+    root->left->right = new node(6);
+
+    root->right->left = new node(3);
+    root->right->right = new node(3);
+
+
+    if (isSumTree(root))
+        cout << "Tree is a Sum Tree" << endl;
+    else
+        cout << "Tree is not a Sum Tree" << endl;
+
+
+    return 0;
 }
