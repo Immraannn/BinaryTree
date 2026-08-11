29d6580c8eb1eb41f2ea5ae38648660a580990bf
Md Imran Shakeel <imshkl2005@gmail.com>
2026-08-12 00:36:40 +0530

    Refactor countLeafNodes by removing comments
    Removed comments to streamline the countLeafNodes function.

--- countleaf03.cpp
+++ countleaf03.cpp
@@ -1,21 +1,11 @@
-// Function to count total leaf nodes
+
 int countLeafNodes(node* root)
 {
-    // Base Case: Empty tree
     if (root == NULL)
         return 0;
-
-    // If current node has no children,
-    // it is a leaf node
     if (root->left == NULL && root->right == NULL)
         return 1;
-
-    // Count leaf nodes in left subtree
     int leftLeaves = countLeafNodes(root->left);
-
-    // Count leaf nodes in right subtree
     int rightLeaves = countLeafNodes(root->right);
-
-    // Total leaf nodes
     return leftLeaves + rightLeaves;
 }
